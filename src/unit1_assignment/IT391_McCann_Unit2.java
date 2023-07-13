a724977ea0b96c5d59875cf578e01a06191ea565
Blake McCann <blake_mccann@yahoo.com>
2023-07-13 10:10:16 -0500

    Adding Java Files to GitHub repo

---
+++ src/unit1_assignment/IT391_McCann_Unit2.java
@@ -0,0 +1,53 @@
+package unit1_assignment;
+
+public class IT391_McCann_Unit2 {
+	
+	//*********************************************************
+
+	//****Assignment 1, Section 1
+
+	//*********************************************************
+	
+	static int fibonacci(int n) {
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+	
+	
+	//*********************************************************
+
+	//****Assignment 1, Section 2
+
+	//*********************************************************	
+	
+	
+	static int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		return n * factorial(n-1);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println("************Section 1***************");
+		System.out.println();
+		System.out.println("The Fibonacci value of 10 is: " + fibonacci(10));
+		
+		System.out.println();
+		System.out.println("************Section 2***************");
+		System.out.println();
+		for(int i = 1; i <= 4; i++) {
+		System.out.println(i+"! is: "+factorial(i));
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ src/unit2_assignment/IT391_McCann_Unit2.java
@@ -0,0 +1,10 @@
+package unit2_assignment;
+
+public class IT391_McCann_Unit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
